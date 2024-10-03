63de15bc374e164101edaa4c24d085baee4175c5
KiSSel3 <andreyk042004@gmail.com>
2024-10-03 21:55:17 +0300

    Added base exceptions

---
+++ src/Services/IdentityService/IdentityService.BLL/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,9 @@
+namespace IdentityService.BLL.Exceptions;
+
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException() : base() { }
+    public EntityNotFoundException(string message) : base(message) { }
+    public EntityNotFoundException(string entityName, Guid id)
+        : base($"Can't find entity of type {entityName} with ID {id}.") { }
+}
