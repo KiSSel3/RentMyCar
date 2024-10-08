1e82f0f54ea03ccbb76423b3c631bcf3b7847561
KiSSel3 <andreyk042004@gmail.com>
2024-10-08 19:33:39 +0300

    Added models

---
+++ src/Services/CarManagementService/CarManagementService.Domain/Models/Location.cs
@@ -0,0 +1,8 @@
+namespace CarManagementService.Domain.Models;
+
+public class Location
+{
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string Building { get; set; }
+}
