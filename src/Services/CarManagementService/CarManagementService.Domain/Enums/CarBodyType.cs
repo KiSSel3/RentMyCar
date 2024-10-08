89f9b76260752414b5c3ad6097e2ac171319c3dc
KiSSel3 <andreyk042004@gmail.com>
2024-10-08 19:25:32 +0300

    Added enums

---
+++ src/Services/CarManagementService/CarManagementService.Domain/Enums/CarBodyType.cs
@@ -0,0 +1,20 @@
+namespace CarManagementService.Domain.Enums;
+
+public enum CarBodyType
+{
+    Convertible,
+    Campervan,
+    Limousine,
+    Crossover,
+    Hatchback,
+    Liftback,
+    Microcar,
+    Roadster,
+    Minivan,
+    Spyder,
+    Wagon,
+    Truck,
+    Coupe,
+    Van,
+    SUV
+}
---
+++ src/Services/CarManagementService/CarManagementService.Domain/Enums/CarTransmissionType.cs
@@ -0,0 +1,8 @@
+namespace CarManagementService.Domain.Enums;
+
+public enum CarTransmissionType
+{
+    SemiAutomatic,
+    Automatic,
+    Manual
+}
