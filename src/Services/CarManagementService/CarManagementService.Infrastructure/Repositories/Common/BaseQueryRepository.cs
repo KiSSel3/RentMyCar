b6784aa97bd56ec566f2d415ee25aebe6100be0b
KiSSel3 <andreyk042004@gmail.com>
2024-10-09 22:01:11 +0300

    Base repository implementation

---
+++ src/Services/CarManagementService/CarManagementService.Infrastructure/Repositories/Common/BaseQueryRepository.cs
@@ -0,0 +1,29 @@
+using CarManagementService.Domain.Abstractions.BaseRepositories;
+using CarManagementService.Domain.Entities;
+using CarManagementService.Infrastructure.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarManagementService.Infrastructure.Repositories.Common;
+
+public class BaseQueryRepository<TEntity> : CommandRepository<TEntity>, IBaseQueryRepository<TEntity>
+    where TEntity : BaseEntity
+{
+    private readonly DbSet<TEntity> _dbSet;
+    
+    public BaseQueryRepository(ApplicationDbContext context) : base(context)
+    {
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbSet.Where(e => !e.IsDeleted)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbSet.FirstOrDefaultAsync(e => e.Id == id && !e.IsDeleted, cancellationToken);
+    }
+}
---
+++ src/Services/CarManagementService/CarManagementService.Infrastructure/Repositories/Common/SpecificationQueryRepository.cs
@@ -0,0 +1,33 @@
+using CarManagementService.Domain.Abstractions.BaseRepositories;
+using CarManagementService.Domain.Abstractions.Specifications;
+using CarManagementService.Domain.Entities;
+using CarManagementService.Infrastructure.Infrastructure;
+using CarManagementService.Infrastructure.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarManagementService.Infrastructure.Repositories.Common;
+
+public class SpecificationQueryRepository<TEntity> : CommandRepository<TEntity>, ISpecificationQueryRepository<TEntity>
+    where TEntity : BaseEntity
+{
+    private readonly DbSet<TEntity> _dbSet;
+    
+    public SpecificationQueryRepository(ApplicationDbContext context) : base(context)
+    {
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default)
+    {
+        return await SpecificationEvaluator.GetQuery(_dbSet, specification)
+            .Where(e=>!e.IsDeleted)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<TEntity> FirstOrDefault(ISpecification<TEntity> specification, CancellationToken cancellationToken = default)
+    {
+        return await SpecificationEvaluator.GetQuery(_dbSet, specification)
+            .FirstOrDefaultAsync(e=>!e.IsDeleted, cancellationToken);
+    }
+}
