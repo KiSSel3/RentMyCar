13931170cb7a992768110f0e66cc1b11d7cba708
KiSSel3 <andreyk042004@gmail.com>
2024-10-16 16:56:49 +0300

    Added CarModel controllers

---
+++ src/Services/CarManagementService/CarManagementService.Presentation/Areas/Admin/Controllers/CarModelController.cs
@@ -0,0 +1,54 @@
+using AutoMapper;
+using CarManagementService.Application.UseCases.Commands.CarModel.CreateCarModel;
+using CarManagementService.Application.UseCases.Commands.CarModel.DeleteCarModel;
+using CarManagementService.Application.UseCases.Commands.CarModel.UpdateCarModel;
+using CarManagementService.Presentation.Models.DTOs.CarModel;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarManagementService.Presentation.Areas.Admin.Controllers;
+
+[ApiController]
+[Route("api/car-model")]
+public class CarModelController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public CarModelController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+    
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateCarModelAsync([FromBody] CarModelRequestDTO request, CancellationToken cancellationToken = default)
+    {
+        var command = _mapper.Map<CreateCarModelCommand>(request);
+        
+        await _mediator.Send(command, cancellationToken);
+        
+        return NoContent();
+    }
+    
+    [HttpPut("update/{id}")]
+    public async Task<IActionResult> UpdateCarModelAsync(Guid id, [FromBody] CarModelRequestDTO request, CancellationToken cancellationToken = default)
+    {
+        var command = _mapper.Map<UpdateCarModelCommand>(request);
+        command.Id = id;
+        
+        await _mediator.Send(command, cancellationToken);
+        
+        return NoContent();
+    }
+    
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeleteCarModelAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var command = new DeleteCarModelCommand { Id = id };
+        
+        await _mediator.Send(command, cancellationToken);
+        
+        return NoContent();
+    }
+}
---
+++ src/Services/CarManagementService/CarManagementService.Presentation/Controllers/CarModelController.cs
@@ -0,0 +1,75 @@
+using AutoMapper;
+using CarManagementService.Application.UseCases.Queries.CarModel.GetAllCarModels;
+using CarManagementService.Application.UseCases.Queries.CarModel.GetCarModelByBrandIdAndName;
+using CarManagementService.Application.UseCases.Queries.CarModel.GetCarModelById;
+using CarManagementService.Application.UseCases.Queries.CarModel.GetCarModelsByBrandId;
+using CarManagementService.Application.UseCases.Queries.CarModel.GetCarModelsByName;
+using CarManagementService.Presentation.Models.DTOs.CarModel;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarManagementService.Presentation.Controllers;
+
+[ApiController]
+[Route("api/car-model")]
+public class CarModelController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public CarModelController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+    
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllCarModelsAsync(CancellationToken cancellationToken = default)
+    {
+        var query = new GetAllCarModelsQuery();
+        
+        var result = await _mediator.Send(query, cancellationToken);
+        
+        return Ok(result);
+    }
+    
+    [HttpGet("get-by-id/{id}")]
+    public async Task<IActionResult> GetCarModelByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var query = new GetCarModelByIdQuery { Id = id };
+        
+        var result = await _mediator.Send(query, cancellationToken);
+        
+        return Ok(result);
+    }
+
+    [HttpGet("get-by-name/{name}")]
+    public async Task<IActionResult> GetCarModelsByNameAsync(string name, CancellationToken cancellationToken = default)
+    {
+        var query = new GetCarModelsByNameQuery { Name = name };
+        
+        var result = await _mediator.Send(query, cancellationToken);
+        
+        return Ok(result);
+    }
+    
+    [HttpGet("get-by-brand-id/{id}")]
+    public async Task<IActionResult> GetCarModelsByBrandIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var query = new GetCarModelsByBrandIdQuery() { BrandId = id };
+        
+        var result = await _mediator.Send(query, cancellationToken);
+        
+        return Ok(result);
+    }
+    
+    [HttpGet("get-by-parameters")]
+    public async Task<IActionResult> GetCarModelByParametersAsync([FromBody] CarModelParametersRequestDTO request, CancellationToken cancellationToken = default)
+    {
+        var query = _mapper.Map<GetCarModelByBrandIdAndNameQuery>(request);
+        
+        var result = await _mediator.Send(query, cancellationToken);
+        
+        return Ok(result);
+    }
+}
