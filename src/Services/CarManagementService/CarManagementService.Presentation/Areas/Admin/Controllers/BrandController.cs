201756aa21797dbe2fb670210fb2b768405b7541
KiSSel3 <andreyk042004@gmail.com>
2024-10-16 16:39:42 +0300

    Update BrandController

--- src/Services/CarManagementService/CarManagementService.Presentation/Areas/Admin/Controllers/BrandController.cs
+++ src/Services/CarManagementService/CarManagementService.Presentation/Areas/Admin/Controllers/BrandController.cs
@@ -1,32 +1,49 @@
+using AutoMapper;
 using CarManagementService.Application.UseCases.Commands.Brand.CreateBrand;
 using CarManagementService.Application.UseCases.Commands.Brand.DeleteBrand;
+using CarManagementService.Application.UseCases.Commands.Brand.UpdateBrand;
+using CarManagementService.Presentation.Models.DTOs.Brand;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarManagementService.Presentation.Areas.Admin.Controllers;
 
-//TODO: Add update and request dtos
 [ApiController]
 [Route("api/brand")]
 public class BrandController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
 
-    public BrandController(IMediator mediator)
+    public BrandController(IMediator mediator, IMapper mapper)
     {
         _mediator = mediator;
+        _mapper = mapper;
     }
     
     [HttpPost("create")]
-    public async Task<IActionResult> CreateBrand([FromBody] CreateBrandCommand command, CancellationToken cancellationToken = default)
+    public async Task<IActionResult> CreateBrandAsync([FromBody] BrandRequestDTO request, CancellationToken cancellationToken = default)
     {
+        var command = _mapper.Map<CreateBrandCommand>(request);
+        
+        await _mediator.Send(command, cancellationToken);
+        
+        return NoContent();
+    }
+    
+    [HttpPut("update/{id}")]
+    public async Task<IActionResult> UpdateBrandAsync(Guid id, [FromBody] BrandRequestDTO request, CancellationToken cancellationToken = default)
+    {
+        var command = _mapper.Map<UpdateBrandCommand>(request);
+        command.Id = id;
+        
         await _mediator.Send(command, cancellationToken);
         
         return NoContent();
     }
     
     [HttpDelete("delete/{id}")]
-    public async Task<IActionResult> DeleteBrand(Guid id, CancellationToken cancellationToken = default)
+    public async Task<IActionResult> DeleteBrandAsync(Guid id, CancellationToken cancellationToken = default)
     {
         var command = new DeleteBrandCommand { Id = id };
         
