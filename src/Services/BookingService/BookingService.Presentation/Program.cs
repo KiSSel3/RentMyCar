57f4b0dc48ae7c861e53f51f789db890346effbc
KiSSel3 <andreyk042004@gmail.com>
2024-10-23 21:10:45 +0300

    Update config

--- src/Services/BookingService/BookingService.Presentation/Program.cs
+++ src/Services/BookingService/BookingService.Presentation/Program.cs
@@ -1,31 +1,15 @@
-using BookingService.BLL;
-using BookingService.DAL;
+using BookingService.Presentation.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddDataAccessLayerServices(builder.Configuration);
-builder.Services.AddBusinessLogicLayerServices(builder.Configuration);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.AddLogging()
+    .AddServices()
+    .ConfigureJWT()
+    .AddAuthentication()
+    .AddSwaggerDocumentation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+app.AddSwagger()
+    .AddApplicationMiddleware()
+    .Run();
