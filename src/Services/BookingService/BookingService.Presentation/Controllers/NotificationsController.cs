5e35f942c2fcaaa976ab515aaf5322e28ef98988
KiSSel3 <andreyk042004@gmail.com>
2024-10-23 20:56:48 +0300

    Added controller for notifications

---
+++ src/Services/BookingService/BookingService.Presentation/Controllers/NotificationsController.cs
@@ -0,0 +1,44 @@
+using BookingService.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingService.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController : ControllerBase
+{
+    private readonly INotificationService _notificationService;
+
+    public NotificationsController(INotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    [HttpGet("get-by-id/{notificationId}")]
+    public async Task<ActionResult> GetNotificationById(Guid notificationId, CancellationToken cancellationToken)
+    {
+        var notification = await _notificationService.GetByIdAsync(notificationId, cancellationToken);
+        return Ok(notification);
+    }
+
+    [HttpGet("get-by-user-id/{userId}")]
+    public async Task<ActionResult> GetNotificationsByUserId(Guid userId, CancellationToken cancellationToken)
+    {
+        var notifications = await _notificationService.GetByUserIdAsync(userId, cancellationToken);
+        return Ok(notifications);
+    }
+
+    [HttpGet("get-unsent")]
+    public async Task<ActionResult> GetUnsentNotifications(CancellationToken cancellationToken)
+    {
+        var notifications = await _notificationService.GetUnsentNotificationsAsync(cancellationToken);
+        return Ok(notifications);
+    }
+
+    [HttpGet("get-unsent-by-user-id/{userId}")]
+    public async Task<ActionResult> GetUnsentNotificationsByUserId(Guid userId, CancellationToken cancellationToken)
+    {
+        var notifications = await _notificationService.GetUnsentNotificationsByUserIdAsync(userId, cancellationToken);
+        return Ok(notifications);
+    }
+}
