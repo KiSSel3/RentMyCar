89f6a562e7d89376d54e59b87a00400c6f3f7047
KiSSel3 <andreyk042004@gmail.com>
2024-12-11 23:04:36 +0300

    Update DeleteReview

--- src/Services/CarManagementService/CarManagementService.Application/UseCases/Review/Commands/DeleteReview/DeleteReviewCommandHandler.cs
+++ src/Services/CarManagementService/CarManagementService.Application/UseCases/Review/Commands/DeleteReview/DeleteReviewCommandHandler.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using CarManagementService.Application.Exceptions;
+using CarManagementService.Application.Models.DTOs;
 using CarManagementService.Domain.Data.Entities;
 using CarManagementService.Domain.Repositories;
 using CarManagementService.Domain.Specifications.Review;
@@ -7,20 +9,23 @@ using Microsoft.Extensions.Logging;
 
 namespace CarManagementService.Application.UseCases.Review.Commands.DeleteReview;
 
-public class DeleteReviewCommandHandler : IRequestHandler<DeleteReviewCommand>
+public class DeleteReviewCommandHandler : IRequestHandler<DeleteReviewCommand, ReviewDTO>
 {
     private readonly IReviewRepository _repository;
+    private readonly IMapper _mapper;
     private readonly ILogger<DeleteReviewCommandHandler> _logger;
 
     public DeleteReviewCommandHandler(
         IReviewRepository repository,
+        IMapper mapper,
         ILogger<DeleteReviewCommandHandler> logger)
     {
         _repository = repository;
         _logger = logger;
+        _mapper = mapper;
     }
 
-    public async Task Handle(DeleteReviewCommand request, CancellationToken cancellationToken)
+    public async Task<ReviewDTO> Handle(DeleteReviewCommand request, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Starting to delete review with ID: {ReviewId}", request.Id);
 
@@ -35,5 +40,7 @@ public class DeleteReviewCommandHandler : IRequestHandler<DeleteReviewCommand>
         await _repository.DeleteAsync(review, cancellationToken);
 
         _logger.LogInformation("Successfully deleted review with ID: {ReviewId}", request.Id);
+        
+        return _mapper.Map<ReviewDTO>(review);
     }
 }
